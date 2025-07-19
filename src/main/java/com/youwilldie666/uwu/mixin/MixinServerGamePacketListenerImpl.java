36cf78d0c9356db6059c71f4c948551cd50ea77b
YOUWILLDIE666 <youwilldie6.066@gmail.com>
2025-07-19 16:30:29 +0300

    `player` must be public (`ServerGamePacketListenerImpl@239`)

--- src/main/java/com/youwilldie666/uwu/mixin/MixinServerGamePacketListenerImpl.java
+++ src/main/java/com/youwilldie666/uwu/mixin/MixinServerGamePacketListenerImpl.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public abstract class MixinServerGamePacketListenerImpl {
 
     @Shadow
-    private ServerPlayer player;
+    public ServerPlayer player;
 
     protected MixinServerGamePacketListenerImpl(ServerPlayer player) {
         this.player = player;
