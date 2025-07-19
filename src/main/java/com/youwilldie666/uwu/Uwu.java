abfea2f000eff5e6f84b121c10f41dd61ad03434
YOUWILLDIE666 <youwilldie6.066@gmail.com>
2025-07-19 17:50:07 +0300

    rfmt

--- src/main/java/com/youwilldie666/uwu/Uwu.java
+++ src/main/java/com/youwilldie666/uwu/Uwu.java
@@ -1,9 +1,7 @@
 package com.youwilldie666.uwu;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,15 +11,12 @@ public class Uwu {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "uwu";
 
-    public Uwu(@NotNull IEventBus modEventBus, @NotNull ModContainer modContainer) {
+    public Uwu(@NotNull IEventBus modEventBus) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-       // NeoForge.EVENT_BUS.register(this);
-
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {}
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
 
 }
